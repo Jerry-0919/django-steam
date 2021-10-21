83637fe073397943ca41f7e3ce7a6444632f7c29
unknown <superdev0919@outlook.com>
2021-10-21 04:42:09 -0700

    url routing

--- screenshot_generator/urls.py
+++ screenshot_generator/urls.py
@@ -19,4 +19,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('app.urls')),
+    path('v2/', include('app.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
