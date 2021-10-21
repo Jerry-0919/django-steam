3b3fb16143f739e7a62b2304c548375ad3e819de
unknown <superdev0919@outlook.com>
2021-10-21 02:51:42 -0700

    fixed syntax error

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,5 @@ from app import views
 
 urlpatterns = [
     path('', views.get_screenshot, name='get_screenshot'),
-    path('v2', view.get_v2_screenshot, name='get_v2_screenshot')
+    path('v2', views.get_v2_screenshot, name='get_v2_screenshot')
 ]
